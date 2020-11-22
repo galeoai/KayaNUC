b79ef4a682022a9352b35bb5b2fe156db39e53a0
Or Dicker <or.dicker@gmail.com>
2020-11-22 14:12:24 +0200

    WIP, fix bug in the kernel

--- cuda/main.cpp
+++ cuda/main.cpp
@@ -1,6 +1,7 @@
 #include "nuc_kernel.h"
 #include "opencv2/opencv.hpp"
 #include "../read_tables/read_nuc.h"
+#include <iostream>
 
 using namespace cv;
 
@@ -8,22 +9,30 @@ using namespace cv;
 
 int main(int argc, char *argv[]) {
     // open img from file
-    Mat img = imread("./data/images/raw.tif", IMREAD_ANYDEPTH);
+    Mat img = imread("../data/images/raw.tif", IMREAD_ANYDEPTH);
 
     float *offset, *gain;
     int *out;
 
     offset = (float *)malloc(N * sizeof(float));
-    read_csv("./data/NUC/offset.csv", offset);
+    read_csv("../data/NUC/offset.csv", offset);
     gain = (float *)malloc(N * sizeof(float));
-    read_csv("./data/NUC/gain.csv", gain);
+    read_csv("../data/NUC/gain.csv", gain);
+    
     // Mat to *int
-    out = img.ptr<int>(0);
+    //out = img.ptr<int>(0);
 
+    out = (int *)malloc(N * sizeof(int));
+    for (int i = 0; i < N; ++i)
+    {
+	out[i] = 30;
+    };
+
+    
     // run the NUC on the GPU
+    std::cout << img.type() << "hi!!!" << std::endl;
     nucCaller(out, gain, offset, N);
-
-    imwrite("./data/images/NUC.tif", img);
+    //imwrite("../data/images/NUC.tif", img);
 
     printf("done!\n");
 
--- cuda/nuc_kernel.cu
+++ cuda/nuc_kernel.cu
@@ -1,33 +1,37 @@
 #include "nuc_kernel.h"
+#include <stdio.h>
 
-#define THREADS 512
+#define THREADS 256
 
-__global__ void nuc(int *out, float *gain, float *offset, int size) {
+__global__ void nuc(int *out,const float *gain,const float *offset, int size) {
     int i = blockIdx.x*blockDim.x + threadIdx.x;
-    if (i<size) out[i] =(int)gain[i]*(out[i] - offset[i]);
+    if (i<size)	out[i] = (int)(gain[i]*(out[i] - offset[i]));
 }
 
 
 void nucCaller(int *out, float *gain, float *offset, int n){
     // copy offset to device
     float *d_offset = NULL;
-    cudaMalloc((void **)&d_offset, n*n*sizeof(float));
-    cudaMemcpy(d_offset, offset, n*n*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMalloc((void **)&d_offset, n*sizeof(float));
+    cudaMemcpy(d_offset, offset, n*sizeof(float), cudaMemcpyHostToDevice);
     // copy gain to device
     float *d_gain = NULL;
-    cudaMalloc((void **)&d_gain, n*n*sizeof(float));
-    cudaMemcpy(d_gain, gain, n*n*sizeof(float), cudaMemcpyHostToDevice);
-
+    cudaMalloc((void **)&d_gain, n*sizeof(float));
+    cudaMemcpy(d_gain, gain, n*sizeof(float), cudaMemcpyHostToDevice);
     int *d_out = NULL;
-    cudaMalloc((void **)&d_out, n*n*sizeof(int));
-    cudaMemcpy(d_out, out, n*n*sizeof(int), cudaMemcpyHostToDevice);
-
+    cudaMalloc((void **)&d_out, n*sizeof(int));
+    cudaMemcpy(d_out, out, n*sizeof(int), cudaMemcpyHostToDevice);
     int threadsPerBlock = THREADS;
     int blocksPerGrid =(n + threadsPerBlock - 1) / threadsPerBlock;
 
-    nuc<<<blocksPerGrid,threadsPerBlock>>>(d_out, d_gain, d_offset,n);
-    cudaMemcpy(out, d_out, n*n*sizeof(int), cudaMemcpyDeviceToHost);
+    printf("%d, %d, %d, %d, %f, %f\n",
+	   out[0], out[1], out[2], out[3], offset[4], gain[5]);
 
+    nuc<<<blocksPerGrid,threadsPerBlock>>>(d_out, d_gain, d_offset,n);
+    cudaMemcpy(out, d_out, n*sizeof(int), cudaMemcpyDeviceToHost);
+
+    printf("%d, %d, %d, %d, %f, %f\n",
+	   out[0], out[1], out[2], out[3], offset[4], gain[5]);
     cudaFree(d_out);
     cudaFree(d_offset);
     cudaFree(d_gain);
